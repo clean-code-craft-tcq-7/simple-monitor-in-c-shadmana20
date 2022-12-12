617a0808ce8da36f65da17243e2d2afcecf6322b
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-12 11:36:32 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,12 +3,14 @@
 
 #define AreTemperatureInRange(X) (X < 0 || X > 45) ? 0 : 1
 
-#define AreSOCInRange(X) (X<20 || X>80) ? 0 : 1
+//#define AreSOCInRange(X) (X<20 || X>80) ? 0 : 1
 
-#define AreChargeRateInRange(X) (X > 0.8 )? 0 : 1
+//#define AreChargeRateInRange(X) (X > 0.8 )? 0 : 1
+
+#define AreChargeRateAndSOCInRange(X,Y) (((X<20 || X>80) && (Y > 0.8 )) ? 0 : 1)
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(AreTemperatureInRange(temperature) && AreChargeRateInRange(chargeRate) && AreSOCInRange(soc))
+  if(AreTemperatureInRange(temperature) && AreChargeRateAndSOCInRange(soc,chargeRate))
   {
     return 0;
   }
