2c1b3c528c42eed74cc9627f70ae5da3e21e7e8e
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-15 11:39:36 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -33,7 +33,7 @@ int main()
  
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
-  assert(batteryIsOk(46, 81, 0.8));
+  assert(!batteryIsOk(46, 81, 0.8));
   assert(batteryIsOk(44, 79, 0.7));
   assert(batteryIsOk(0, 0, 0.7));
 
