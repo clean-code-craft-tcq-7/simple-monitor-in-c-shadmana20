951804bd09430fff64f124259e679474284aa74f
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-16 22:20:36 +0530

    Create BatteryHealthMonitoring.h

---
+++ BatteryHealthMonitoring.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+typedef enum {
+    LOW_SOC_BREACH,
+    LOW_SOC_WARNING,
+    SOC_NORMAL,
+    HIGH_SOC_WARNING,
+    HIGH_SOC_BREACH,
+    SOC_UNDEFINED,
+    RANGE_MAX
+} WarningRanges;
+
+
+typedef enum {
+    DEFAULT,
+    ENGLISH,
+    GERMAN,
+    CHINA
+}LanguageUsed;
+
+
+typedef enum {
+    Approaching_Discharge,
+    Approaching_Peak
+} WarningWithTolerance;
+
+
+void MonitorHealthWithTolerance(int tolerance , int MinThreshold, int MaxThrshold , int* DrainRange , int* PeakRange);
+void ConvertLanguageIndex (LanguageUsed Language);
+WarningRanges BatteryHelathMonitor(int soc);
+const char* WarningMessageToDisplay[RANGE_MAX] = {};
+
+const char* WarningMessageEN[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"};
+const char* WarningMessageDE[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"};
+const char* WarningMessageCN[RANGE_MAX] = {"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"};
+
+
+
+
