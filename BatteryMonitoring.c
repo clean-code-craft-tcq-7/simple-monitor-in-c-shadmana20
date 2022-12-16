3079a2862ddb48b417a46dfeba3187c44d9eda27
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-16 23:14:21 +0530

    Update BatteryMonitoring.c

--- BatteryMonitoring.c
+++ BatteryMonitoring.c
@@ -1,8 +1,5 @@
 #include "BatteryHealthMonitoring.h"
 
-const char* WarningMessageEN[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"};
-const char* WarningMessageDE[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"};
-const char* WarningMessageCN[RANGE_MAX] = {"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"};
 
 
 void ConvertLanguageIndex (LanguageUsed Language)
