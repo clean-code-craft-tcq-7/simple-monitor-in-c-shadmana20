0e57e139ab817871d3c09235bf57add6bc40cf41
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-17 02:58:45 +0530

    Update BatteryHealthMonitoring.h

--- BatteryHealthMonitoring.h
+++ BatteryHealthMonitoring.h
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-const char* WarningMessageToDisplay[RANGE_MAX] = {};
-const char* WarningMessageEN[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"};
-const char* WarningMessageDE[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"};
-const char* WarningMessageCN[RANGE_MAX] = {"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"};
 
 typedef enum {
     LOW_SOC_BREACH,
@@ -28,6 +24,11 @@ typedef enum {
     Approaching_Discharge,
     Approaching_Peak
 } WarningWithTolerance;
+
+const char* WarningMessageToDisplay[RANGE_MAX] = {};
+const char* WarningMessageEN[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"};
+const char* WarningMessageDE[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"};
+const char* WarningMessageCN[RANGE_MAX] = {"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"};
 
 
 void MonitorHealthWithTolerance(int tolerance , int MinThreshold, int MaxThrshold , int* DrainRange , int* PeakRange);
