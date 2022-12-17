c1b8750e6a55277197303036d1625d031f0360cb
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-17 18:07:05 +0530

    Update BatteryMonitoring.c

--- BatteryMonitoring.c
+++ BatteryMonitoring.c
@@ -1,12 +1,6 @@
 
 #include "BatteryHealthMonitoring.h"
 
-#define RangeChecker(X,Y,Z) ( X>=Y && X <=Z) ? 1 : 0
-
-
-//char* WarningMessageToDisplay[RANGE_MAX];
-
-
 void MonitorHealthWithTolerance(int tolerance ,int MinThreshold , int MaxThreshold , int *DrainRange , int *PeakRange)
 {
     
@@ -22,26 +16,20 @@ WarningRanges BatteryHelathMonitor(int soc)
     switch(soc)
     {
     case 0 ... 20:        
-    //if(soc>=0 && soc<=20)
     return LOW_SOC_BREACH;
     break;
-    //else if(soc>=21 && soc<=24)
     case 21 ...24:
     return LOW_SOC_WARNING;
     break;
     case 25 ... 75:        
-   // else if(soc>=25 && soc<=75)
     return SOC_NORMAL; 
     break;
-    //else if(soc>=76 && soc<=80)
     case 76 ... 80:        
     return HIGH_SOC_WARNING;
     break;
-    //else if(soc>=81 && soc<=100)
     case 81 ... 100:
     return HIGH_SOC_BREACH;
     break;
-    //else
     default:
     return SOC_UNDEFINED;
     break;
