e0188499ddc4de0e9c5afba332ae80d766909251
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-17 16:52:47 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -71,11 +71,9 @@ int main()
   assert(batteryIsOk(44, 79, 0.7));
   assert(!batteryIsOk(0, 0, 0.7));
   ConvertLanguageIndex(DEFAULT);
-  printf(" %s " , WarningMessageToDisplay[(BatteryHelathMonitor(13))]);
-  //assert(WarningMessageToDisplay[(BatteryHelathMonitor(13))] == "LOW_SOC_WARNING");
+  assert(WarningMessageToDisplay[(BatteryHelathMonitor(13))] == "LOW_SOC_BREACH");
   ConvertLanguageIndex(GERMAN);
-  printf(" %s " , WarningMessageToDisplay[(BatteryHelathMonitor(77))]);
-  //assert(WarningMessageToDisplay[(BatteryHelathMonitor(77))] == "HIGH_SOC_WARNUNG");
+  assert(WarningMessageToDisplay[(BatteryHelathMonitor(77))] == "HIGH_SOC_WARNUNG");
 
 }
 
