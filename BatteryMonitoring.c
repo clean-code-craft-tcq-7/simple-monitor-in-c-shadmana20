785d8f761c818a7382dbef68caf7e74350e90516
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-17 16:43:38 +0530

    Update BatteryMonitoring.c

--- BatteryMonitoring.c
+++ BatteryMonitoring.c
@@ -3,32 +3,6 @@
 
 
 //char* WarningMessageToDisplay[RANGE_MAX];
-const  char* WarningMessageEN[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNING","SOC_NORMAL","HIGH_SOC_WARNING","HIGH_SOC_BREACH","SOC_UNDEFINED"};
-const  char* WarningMessageDE[RANGE_MAX] = {"LOW_SOC_BREACH","LOW_SOC_WARNUNG","SOC_NORMAL","HIGH_SOC_WARNUNG","HIGH_SOC_BREACH","SOC_UNDEFINED"};
-const  char* WarningMessageCN[RANGE_MAX] = {"L_S_BREACHEN","L_S_WARN!!","NORMAL","H_S_WARN!!","H_S_BREACHEN","UNDEFINED"};
-
-void ConvertLanguageIndex (LanguageUsed Language)
-{       
-    char* LocalVariable[RANGE_MAX];
-    
-    for(int i =0;i<RANGE_MAX;i++)
-        {
-            if( Language <GERMAN){
-            LocalVariable[i] = WarningMessageEN[i];
-            }
-            else if (Language == GERMAN)
-            {
-            LocalVariable[i] = WarningMessageDE[i];   
-            }
-            else{
-            LocalVariable[i] = WarningMessageCN[i];
-            }
-        
-          WarningMessageToDisplay[i] = LocalVariable[i];
-        }
-    
-    
-}
 
 
 void MonitorHealthWithTolerance(int tolerance ,int MinThreshold , int MaxThreshold , int *DrainRange , int *PeakRange)
