b3f8bf1fa61bb1244a8705b569e88337f7c2edbb
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-17 15:51:52 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,5 +1,3 @@
-#ifndef BATTERY_HEALTH_MONITORING_H
-#define BATTERY_HEALTH_MONITORING_H
 #include <stdio.h>
 #include <assert.h>
 #include "BatteryHealthMonitoring.h"
@@ -50,4 +48,4 @@ int main()
 }
 
 
-#endif
+
