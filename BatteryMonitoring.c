ac3545116a4ab08e2465fc84dc2c6291a60f1192
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-19 17:06:46 +0530

    Update BatteryMonitoring.c

--- BatteryMonitoring.c
+++ BatteryMonitoring.c
@@ -1,39 +1,99 @@
 
 #include "BatteryHealthMonitoring.h"
 
-void MonitorHealthWithTolerance(int tolerance ,int MinThreshold , int MaxThreshold , int *DrainRange , int *PeakRange)
+#define TOLERENCEVALUE 5
+#define RangeChecker(X,Y,Z) (X<=Y && X>=Z)?1:0
+
+struct 
 {
+	int DrainRange;
+	int PeakRange;
+}descharge_range;
+
+/*Function Decleration*/
+static WarningRanges ReturnSocBreach(int soc);
+static WarningRanges ReturnSocWarning(int soc);
+static int SocRangeCheck(int soc, int MinRange, int MaxRange);
+static void CalculateDrianPeakThreshold(int MinThreshold , int MaxThreshold);
+
+static int SocRangeCheck(int soc, int MinRange, int MaxRange)
+{
+	if(soc <= MinRange)
+	{
+		return 1;
+	}
+	else if(soc>=MaxRange)
+	{
+		return 0;
+	}
+	
+}
+
+static void CalculateDrianPeakThreshold(int MinThreshold , int MaxThreshold)
+{
+    int Value = (int)((TOLERENCEVALUE*MaxThreshold)/100);
     
-    int Value = (int)((tolerance*MaxThreshold)/100);
-    
-    *DrainRange = MinThreshold + Value;
-    *PeakRange  = MaxThreshold - Value;
-    
+     descharge_range.DrainRange = MinThreshold + Value;
+     descharge_range.PeakRange  = MaxThreshold - Value;
+}
+
+WarningWithTolerance PreWarningIndicatorMessage(int input, int MinThreshold , int MaxThreshold )
+{   
+
+    CalculateDrianPeakThreshold(MinThreshold , MaxThreshold);
+	
+    if(input <= descharge_range.DrainRange)	    
+    {
+        return Approaching_Discharge;
+    }
+ 
+    if( input>= descharge_range.PeakRange)
+    {
+        return Approaching_Peak;
+    }
 }
 
 WarningRanges BatteryHelathMonitor(int soc)
 {  
-    switch(soc)
-    {
-    case 0 ... 20:        
-    return LOW_SOC_BREACH;
-    break;
-    case 21 ...24:
-    return LOW_SOC_WARNING;
-    break;
-    case 25 ... 75:        
-    return SOC_NORMAL; 
-    break;
-    case 76 ... 80:        
-    return HIGH_SOC_WARNING;
-    break;
-    case 81 ... 100:
-    return HIGH_SOC_BREACH;
-    break;
-    default:
-    return SOC_UNDEFINED;
-    break;
-            
-    }
+    if(RangeChecker(soc, 81, 21) == 1)
+	{
+		return ReturnSocWarning(soc);
+	}
+	else
+	{
+		return ReturnSocBreach(soc);
+	}
 }
 
+
+static WarningRanges ReturnSocBreach(int soc)
+{
+	 
+	if(SocRangeCheck(soc, 20, 81) == 1)
+	{
+		return LOW_SOC_BREACH;
+	}
+	else
+	{
+		return HIGH_SOC_BREACH;
+	}
+	
+}
+
+static WarningRanges ReturnSocWarning(int soc)
+{
+	
+	WarningRanges Returnstaus = SOC_NORMAL;
+	
+	if(SocRangeCheck(soc, 24, 76) == 1)
+	{
+		Returnstaus = LOW_SOC_WARNING;
+	}
+	else
+	{
+		Returnstaus = HIGH_SOC_WARNING;
+	}
+	
+	return Returnstaus;
+	
+}
